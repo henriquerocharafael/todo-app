568fbfe956edc8528bc7995fbd31802707175d12
henriquerocharafael <rafaelrocha2500@gmail.com>
2024-05-29 16:47:30 -0300

    Mobile layout finished

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,7 +1,6 @@
 import { TodoProps } from "../../App";
 import iconCross from "../../assets/images/icon-cross.svg";
-import { Checkbox } from "./checkbox";
-import { ListStatus } from "./list-status";
+import { Checkbox } from "../Checkbox";
 import styles from "./styles.module.css";
 
 interface TodoListProps {
@@ -25,36 +24,88 @@ export function TodoList({
   todoList,
   status,
 }: TodoListProps) {
+  const statusItems = ["all", "active", "completed"];
+
   return (
     <main>
-      <ul>
-        {filteredList.map((todo) => {
-          return (
-            <li key={todo.id}>
-              <div className={styles.listItem}>
-                <Checkbox onChange={() => handleCheckboxChange(todo.id)} />
-                <div className={todo.completed ? styles.completed : ""}>
-                  {todo.description}
+      <div className={styles.desktop}>
+        <ul>
+          {filteredList.map((todo) => {
+            return (
+              <li key={todo.id}>
+                <div className={styles.listItem}>
+                  <Checkbox onChange={() => handleCheckboxChange(todo.id)} />
+                  <div className={todo.completed ? styles.completed : ""}>
+                    {todo.description}
+                  </div>
                 </div>
-              </div>
-              <img
-                className={styles.iconCross}
-                src={iconCross}
-                onClick={() => deleteTodo(todo.id)}
-                alt="Delete item icon"
-              />
-            </li>
-          );
-        })}
-      </ul>
+                <img
+                  className={styles.iconCross}
+                  src={iconCross}
+                  onClick={() => deleteTodo(todo.id)}
+                  alt="Delete item icon"
+                />
+              </li>
+            );
+          })}
+        </ul>
 
-      <ListStatus
-        setStatus={setStatus}
-        itemsLeft={itemsLeft}
-        todoList={todoList}
-        setTodoList={setTodoList}
-        status={status}
-      />
+        <div className={styles.listStatus}>
+          <div className={styles.items}>
+            <span>{itemsLeft.length}</span> items left
+          </div>
+          <div className={styles.status}>
+            <span
+              className={statusItems[0] === status ? styles.active : ""}
+              onClick={() => setStatus("all")}
+            >
+              {statusItems[0]}
+            </span>
+            <span
+              className={statusItems[1] === status ? styles.active : ""}
+              onClick={() => setStatus("active")}
+            >
+              {statusItems[1]}
+            </span>
+            <span
+              className={statusItems[2] === status ? styles.active : ""}
+              onClick={() => setStatus("completed")}
+            >
+              {statusItems[2]}
+            </span>
+          </div>
+          <div className={styles.clear}>
+            <span
+              onClick={() =>
+                setTodoList(todoList.filter((todo) => todo.completed === false))
+              }
+            >
+              clear completed
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.statusMobile}>
+        <span
+          className={statusItems[0] === status ? styles.active : ""}
+          onClick={() => setStatus("all")}
+        >
+          {statusItems[0]}
+        </span>
+        <span
+          className={statusItems[1] === status ? styles.active : ""}
+          onClick={() => setStatus("active")}
+        >
+          {statusItems[1]}
+        </span>
+        <span
+          className={statusItems[2] === status ? styles.active : ""}
+          onClick={() => setStatus("completed")}
+        >
+          {statusItems[2]}
+        </span>
+      </div>
     </main>
   );
 }
