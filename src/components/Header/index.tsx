149c825f2f79069b9e024a255a2583c0d63fbff8
henriquerocharafael <rafaelrocha2500@gmail.com>
2024-06-05 17:02:45 -0300

    Toggle light and dark mode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import iconSun from "../../assets/images/icon-sun.svg";
+import iconMoon from "../../assets/images/icon-moon.svg";
 import styles from "./styles.module.css";
 
 interface HeaderProps {
@@ -10,12 +11,20 @@ interface HeaderProps {
 
 export function Header({ addTodo, todoItem, handleInputChange }: HeaderProps) {
   const disabled = todoItem.length <= 0;
+  const [themeIcon, setThemeIcon] = useState(iconSun);
+
+  function changeTheme() {
+    const html = document.querySelector("html");
+    html?.classList.toggle("light-mode");
+
+    setThemeIcon(themeIcon == iconSun ? iconMoon : iconSun);
+  }
 
   return (
     <header>
       <div className={styles.titleWrapper}>
         <h1>todo</h1>
-        <img src={iconSun} alt="Theme icon" />
+        <img src={themeIcon} alt="Theme icon" onClick={changeTheme} />
       </div>
 
       <div className={styles.inputWrapper}>
